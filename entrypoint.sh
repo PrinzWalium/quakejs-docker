cd8e41981cfcc24cb72bfb125f58e239ccb65154
PrinzWalium <90461995+PrinzWalium@users.noreply.github.com>
2025-12-09 09:15:39 +0100

    Update entrypoint.sh to redirect content server
    Redirect content server to localhost by updating /etc/hosts.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Redirect content server to localhost
+echo "127.0.0.1 content.quakejs.com" >> /etc/hosts
+
 cd /var/www/html
 
 sed -i "s/'quakejs:/window.location.hostname + ':/g" index.html
